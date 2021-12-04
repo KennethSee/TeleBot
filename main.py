8d93820506126eae10d5ed0a9670c46b476c615e
Kenneth See <kennethseede@gmail.com>
2021-12-05 03:05:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 import telebot
+from flask import Flask, request
 
 from storage import add_score, reduce_score
 from db import DB
 
 
 API_KEY = os.getenv('API_KEY')
+PORT = int(os.environ.get('PORT', 5000))
 bot = telebot.TeleBot(API_KEY)
 db = DB('urkmebot-firebase-adminsdk-ezt3s-c8553cf80a.json')
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['greet'])
@@ -38,4 +41,20 @@ def social_credit_counter(message):
             reduce_score(db, username, name, 20)
         bot.reply_to(message, f'{name}, your social credit is now {db.get_social_credit_score(username)}')
 
-bot.infinity_polling()
+@server.route('/' + API_KEY, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://lit-cliffs-68545.herokuapp.com/' + API_KEY)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
