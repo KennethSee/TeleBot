7de43cf9dafa790683ca5c3763a74e12d87d8f63
Kenneth See <kennethseede@gmail.com>
2021-12-05 11:51:19 +0800

    Update db.py

--- db.py
+++ db.py
@@ -9,6 +9,7 @@ class DB:
             firebase_admin.initialize_app(cd)
         self.db = firestore.client()
         self.social_credit = self.db.collection(u'social_credit')
+        self.chats = self.db.collection(u'chats')
 
     def change_social_credit_score(self, username, name, score_change):
         ref = self.social_credit.document(username)
@@ -31,3 +32,21 @@ class DB:
             return score
         else:
             return 0
+
+    def get_user_name(self, username):
+        ref = self.social_credit.document(username)
+        doc = ref.get()
+        if doc.exists:
+            name = ref.get().to_dict()['name']
+            return name
+        else:
+            return None
+
+    def get_chat_members(self, chat_id) -> list:
+        ref = self.chats.document(str(chat_id))
+        doc = ref.get()
+        if doc.exists:
+            members = list(doc.to_dict()['members'])
+            return members
+        else:
+            return []
