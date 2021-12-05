da9747412d346f2831775dc2fc29d5c7ef7721f3
Kenneth See <kennethseede@gmail.com>
2021-12-05 11:51:01 +0800

    Update storage.py

--- storage.py
+++ storage.py
@@ -7,3 +7,21 @@ def add_score(db: DB, username, name, score):
 
 def reduce_score(db: DB, username, name, score):
     db.change_social_credit_score(username, name, score * -1)
+
+def get_ranking(db: DB, chat_id):
+    members = db.get_chat_members(chat_id)
+    if len(members) == 0:
+        return None
+    scores = []
+    for member in members:
+        if member_name is not None:
+            member_name = db.get_user_name(member)
+            member_score = db.get_social_credit_score(member)
+            scores.append((member_name, member_score))
+    scores.sort(key=lambda x: x[1], reverse=True)
+    # generate formated output rank
+    output = 'Leaderboard:\n'
+    for name, score in scores:
+        output += f'{name}: {score}\n'
+
+    return output
